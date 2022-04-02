9679b340772a53c187121d1b6f62b29a88c8a8b3
Nader Innovant <nader@innovant.studio>
2022-04-02 18:32:40 +0100

    ADD:Register Page Design

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Logo, FormRow, Alert } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
 
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+    showAlert: false
+}
 const Register = () => {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDeffault()
+        console.log(e.target)
+    }
     return (
-        <div>Register</div>
+        <Wrapper className='full-page'>
+            <form className='form' onSubmit={onSubmit}>
+                <Logo />
+                <h3 style={{ fontWeight: 600 }}>Login</h3>
+                {values.showAlert && <Alert />}
+                {/* Name Input */}
+                <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
+                {/* Email Input */}
+                <FormRow type='email' name='email' value={values.email} handleChange={handleChange} />
+                {/* Password Input */}
+                <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
+                <button type='submit' className='btn btn-block'>Submit </button>
+            </form>
+        </Wrapper>
     )
 }
 
