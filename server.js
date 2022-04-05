f5ae971ca693de9430db7bc3fa67edae04f3d1f7
Nader Innovant <nader@innovant.studio>
2022-04-06 00:06:48 +0100

    ADD:Register Controller/ User Model

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 import express from "express";
 
 import dotenv from 'dotenv';
+import 'express-async-errors';
+
+//db and authenticateUser
 import connectDB from "./db/connect.js";
 
-//Middlewares
+//routers
+import authRouter from './Routes/authRouter.js';
+import jobsRouter from './Routes/jobsRouter.js';
+
+//middlewares
 import errorHandlerMiddleware from "./middlewares/error-handler.js";
 import notFoundMiddleware from "./middlewares/not-found.js";
+
 dotenv.config()
 
 const app = express();
-
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
     res.send('Welcome!')
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware);
